779a551617cb472d43bdb9c84949906535ee47df
yekang <wb-yekang@rokid.com>
2024-07-17 18:18:50 +0800

    fix: 配置接入子应用

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,11 @@
 import Pages from "@/pages";
-
+// index.js
+import microApp from "@micro-zoe/micro-app";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    microApp.start();
+  }, []);
   return (
     <div className="App">
       <Pages />
